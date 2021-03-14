42538494acd4dbdb28dc768630c099f156adf448
Abdirahman Saed <77121312+Abdirahman23@users.noreply.github.com>
2021-03-14 07:21:14 +0300

    adding html pages and styles

---
+++ netflix.js
@@ -0,0 +1,13 @@
+let signIn = document.querySelector(".sign-in")
+let model = document.querySelector(".model-box")
+console.log("hello")
+
+
+function Model_Show(){
+    model.classList.add("form")
+}
+
+signIn.addEventListener("click" , Model_Show )
+
+
+
